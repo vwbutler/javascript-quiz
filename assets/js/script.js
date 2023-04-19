9e39c89dd33ec725aaa07ea0caea03debfb00064
Virginia Butler <VButler@masc.sc>
2023-04-19 10:46:21 -0400

    first release

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,10 +35,28 @@ var choiceC = document.getElementById("btn2");
 var choiceD = document.getElementById("btn3");
 var answerCheck = document.getElementById("answerCheck");
 
-var questionIndex = 3;
+var questionIndex = 0;
 
-function displayQuiz(questions) {
+function correctAnswer() {
+  console.log("click");
+}
+
+function displayQuiz() {
+  questionTitle.innerHTML = questions[questionIndex].question;
+  choiceA.innerHTML = questions[questionIndex].choices[0];
+  choiceB.innerHTML = questions[questionIndex].choices[1];
+  choiceC.innerHTML = questions[questionIndex].choices[2];
+  choiceD.innerHTML = questions[questionIndex].choices[3];
+  answerCheck.innerHTML = questions[questionIndex].answer;
   console.log(questions[questionIndex].question);
   console.log(questions[questionIndex].choices);
-  console.log(questions.question);
+  console.log(questions[questionIndex].answer);
 }
+
+displayQuiz();
+
+//example
+choiceA.addEventListener("click", correctAnswer);
+choiceB.addEventListener("click", correctAnswer);
+//display function
+//include wording
